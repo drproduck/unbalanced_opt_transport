6dca34135f68ca154b764d4c1e4bfb753a0d804d
drproduck <duckhiem95@yahoo.com>
2020-03-24 15:32:49 +0700

    exp on muot

--- MUOT.py
+++ MUOT.py
@@ -18,15 +18,31 @@ def get_B(C, U, eta):
 
     return np.exp(K / eta)
 
-def f(B, U, R, eta, T):
+def f_dual(B, U, R, eta, T):
     """
     the dual of the entropic-regularized unbalanced OT
     """
     n = len(B.shape)
-    f = eta * np.sum(B)
+    transport = np.sum(B)
+    f = eta * transport
+    margs = []
     for i in range(n):
-        f += T[i] * dotp(np.exp(- U[i] / T[i]) - 1, R[i])
+        margs.append(dotp(np.exp(- U[i] / T[i]), R[i]))
+        f += T[i] * margs[i]
+    return f, transport, margs
 
+
+def f_primal(C, B, U, R, eta, T):
+    """
+    the primal objective with solutions B
+    """
+    n = len(B.shape)
+    f = dotp(C, B)
+    for i in range(n):
+        ai = B.sum(axis=get_except(n, i)).reshape(-1, 1)
+        f += T[i] * get_KL(ai, R[i])
+
+    f -= eta * get_entropy(B)
     return f
 
 def unreg_f(C, B, U, R, eta, T):
@@ -36,7 +52,7 @@ def unreg_f(C, B, U, R, eta, T):
     n = len(B.shape)
     f = dotp(C, B)
     for i in range(n):
-        ai = B.sum(axis=get_except(n, i)).reshape(1, -1)
+        ai = B.sum(axis=get_except(n, i)).reshape(-1, 1)
         f += T[i] * get_KL(ai, R[i])
 
     return f
@@ -55,11 +71,14 @@ def sinkhorn_muot(C, R, eta, T, n_iter=100, early_stop=True):
     n = len(dims)
 
     # collect some stats
-    f_val_list = []
+    f_primal_val_list = []
+    f_dual_val_list = []
     unreg_f_val_list = []
     err_list = []
     time_per_iter_list = []
 
+    # all_solutions
+    U_all = []
     # initial solution
     U = []
     for d in dims:
@@ -69,14 +88,17 @@ def sinkhorn_muot(C, R, eta, T, n_iter=100, early_stop=True):
     # compute before any updates
     B = get_B(C, U, eta)
 
-    f_val = f(B, U, R, eta, T)
-    f_val_list.append(f_val)
+    f_primal_val = f_primal(C, B, U, R, eta, T)
+    f_primal_val_list.append(f_primal_val)
+    f_dual_val = f_dual(B, U, R, eta, T)
+    f_dual_val_list.append(f_dual_val)
     unreg_f_val = unreg_f(C, B, U, R, eta, T)
     unreg_f_val_list.append(unreg_f_val)
 
 
     stop_iter = n_iter
     for i in range(n_iter):
+        sol_it = []
 
         j = i % n
         old = copy(U[j])
@@ -86,14 +108,21 @@ def sinkhorn_muot(C, R, eta, T, n_iter=100, early_stop=True):
         aj = B.sum(axis=get_except(n, j)).reshape(-1, 1)
         U[j] = (U[j] / eta + np.log(R[j]) - np.log(aj)) * (T[j] * eta / (eta + T[j]))
 
+        #save solution of this iter
+        for k in range(n):
+            sol_it.append(copy(U[k]))
+        U_all.append(sol_it)
+
         B = get_B(C, U, eta)
         stop = time()
 
         # post-process
         time_per_iter = stop - start
         time_per_iter_list.append(time_per_iter)
-        f_val = f(B, U, R, eta, T)
-        f_val_list.append(f_val)
+        f_primal_val = f_primal(C, B, U, R, eta, T)
+        f_primal_val_list.append(f_primal_val)
+        f_dual_val, _, _ = f_dual(B, U, R, eta, T)
+        f_dual_val_list.append(f_dual_val)
         unreg_f_val = unreg_f(C, B, U, R, eta, T)
         unreg_f_val_list.append(unreg_f_val)
 
@@ -101,17 +130,19 @@ def sinkhorn_muot(C, R, eta, T, n_iter=100, early_stop=True):
         err = norm1(new - old)
         err_list.append(err)
 
-        if early_stop and err < 1e-10:
-            stop_iter = i + 1
-            break
+#         if early_stop and err < 1e-10:
+#             stop_iter = i + 1
+#             break
 
     info = {}
-    info['f_val_list'] = f_val_list
+    info['f_primal_val_list'] = f_primal_val_list
+    info['f_dual_val_list'] = f_dual_val_list
     info['unreg_f_val_list'] = unreg_f_val_list
     info['err_list'] = err_list
     info['time_per_iter_list'] = time_per_iter_list
     if early_stop:
         info['stop_iter'] = stop_iter
     else: info['stop_iter'] = n_iter
+    info['U_all'] = U_all
 
     return info
--- geo_rate_delta.py
+++ geo_rate_delta.py
@@ -2,20 +2,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 from UOT import *
 import pdb
+import cvxpy as cp
 
 np.random.seed(9999)
 
 nr = 100
 nc = 100
-eta = 0.1
-tau = 10
+eta = 1
+tau = 5
 C = np.random.uniform(low=1, high=10, size=(nr, nc))
 C = (C + C.T) / 2
 r = np.random.uniform(low=0.1, high=1, size=(nr, 1))
 
 c = np.random.uniform(low=0.1, high=1, size=(nc, 1))
 
-u, v, info = sinkhorn_uot(C, r, c, eta=eta, t1=tau, t2=tau, early_stop=False, n_iter=2000)
+def solve_g_dual_cp(C, a, b, eta, tau):
+    u = cp.Variable(shape=a.shape)
+    v = cp.Variable(shape=b.shape)
+
+    u_stack = cp.vstack([u.T for _ in range(nr)])
+    v_stack = cp.hstack([v for _ in range(nc)])
+    print(u_stack.shape, v_stack.shape)
+
+    # obj = eta * cp.sum(cp.multiply(cp.exp(u + v.T) * cp.exp(v).T, 1 / cp.exp(C)))
+    # obj = eta * cp.sum(cp.multiply(cp.exp(u_stack + v_stack), 1 / cp.exp(C)))
+    obj = eta * cp.sum(cp.exp((u_stack + v_stack - C) / eta))
+    obj += tau * cp.sum(cp.multiply(cp.exp(- u / tau), a))
+    obj += tau * cp.sum(cp.multiply(cp.exp(- v / tau), b))
+
+    prob = cp.Problem(cp.Minimize(obj))
+    prob.solve()
+
+    return prob.value, u.value, v.value
+
+opt_val, ustar, vstar = solve_g_dual_cp(C, r, c, eta, tau)
+print(ustar.shape, vstar.shape)
+ustar = ustar.reshape(-1, 1)
+vstar = vstar.reshape(-1, 1)
+
+u, v, info = sinkhorn_uot(C, r, c, eta=eta, t1=tau, t2=tau, early_stop=False, n_iter=1000)
 
 delta_list = []
 geom_list = []
@@ -24,15 +49,15 @@ max_ratio_list = []
 ratio_list = []
 bound_list = []
 
+
 print(info['stop_iter'])
 for i in range(info['stop_iter']):
-    delta = np.max([supnorm(info['u_list'][i] - info['u_list'][-1]), supnorm(info['v_list'][i] - info['v_list'][-1])])
+    delta = np.max([supnorm(info['u_list'][i] - ustar), supnorm(info['v_list'][i] - vstar)])
     delta_list.append(delta)
 
     if i == 0:
         geom_list.append(delta)
         bound_list.append(tau * (np.max([supnorm(np.log(r)), supnorm(np.log(c))]) + np.max([supnorm(C) / eta - np.log(nr), np.log(nr)])))
-        print(bound_list[-1] / geom_list[-1])
     else:
         geom_list.append(geom_list[-1] * (tau / (tau + eta)))
         bound_list.append(bound_list[-1] * (tau / (tau + eta)))
@@ -45,14 +70,21 @@ for i in range(info['stop_iter']):
         ratio = delta_list[-2] / delta_list[-1]
         ratio_list.append(ratio)
 
-print(delta_list)
-# print(geom_list)
+# print(delta_list)
+print(geom_list)
+fig, ax = plt.subplots(2, 1)
+plt.rcParams.update({'font.size': 22})
+# plt.figure(figsize=(10, 8))
+ax[0].plot(range(info['stop_iter']-1), ratio_list, linewidth=2, label='$\Delta_{k-1} / \Delta_{k}$')
+ax[1].plot(range(info['stop_iter']+1), info['f_val_list'], linewidth=2)
+# plt.plot(epsilon_list, [tmp / 1000 for tmp in k_list_empirical_true], "r", linewidth=4, label=r"$k_{true}$")
+# plt.plot(epsilon_list, [tmp / 1000 for tmp in k_list_empirical_first], "g", linewidth=4, label=r"$k_{first}$")
+# plt.plot(epsilon_list, [tmp / 1000 for tmp in k_list_formula], "b", linewidth=4, label=r"$k_{formula}$")
+# plt.xlabel("epsilon")
+plt.xlabel("k (iterations)")
+plt.legend(prop={'size': 30})
 
-# print(info['stop_iter'])
-fig, ax = plt.subplots(1,1)
-
-ax.plot(range(info['stop_iter']-1), ratio_list[1::2], label='$\Delta_{k-1} / \Delta_{k}$')
-ax.set_xlabel('k (iterations)')
-ax.legend()
-
+plt.savefig('delta_rate.eps', bbox_inches='tight')
+# plt.savefig('k_comparison.png', bbox_inches='tight')
 plt.show()
+
