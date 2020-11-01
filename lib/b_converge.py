fixed gradient of l2_semi_uot

lib/b_converge.py
@@ -9,6 +9,7 @@ import ot
 from utils import *
 
 from scipy.sparse import coo_matrix
+from copy import copy
 
 
 def im_text(ax, data):
@@ -44,26 +45,29 @@ a = a.reshape(25, 1)
 # a = np.array([1., 1.]).reshape(-1, 1)
 b = np.zeros((5, 5))
 b[0, 1] = 1.
-b[4, 3] = 1.
-b[4, 0] = 1.
+b[4, 3] = 0.5
+b[4, 0] = 0.5
 b = b.reshape(25, 1)
 a = a + 1e-16
-b = b + 1e-16
+b_init = b + 1e-16
 
 xx = np.arange(5)
 yy = np.arange(5)
 XX, YY = np.meshgrid(xx, yy, indexing='ij')
 supp = np.concatenate((XX.reshape(-1,1), YY.reshape(-1,1)), axis=1)
 
-C = cdist(supp, supp, 'euclidean')**2
+C = cdist(supp, supp, 'euclidean')
+# C = np.zeros((25, 25))
 
-eta = 0.01
-tau = 100.
+eta = 10.
+tau = 10.
 
-def get_beta(b, med):
+def get_beta(b_pr, med):
+    b = copy(b_pr)
+    b[b <= 0] = 1e-16
 
     if med == 'l2':
-        X, beta_l2 = pgd(C, a, b, tau, duals=True)
+        X, beta_l2 = fw(C, a, b, tau, duals=True)
         beta_l2 = beta_l2 / tau
         
         return beta_l2
@@ -83,11 +87,13 @@ def get_beta(b, med):
         return beta_ot
 
     elif med == 'pot':
+        b = b.flatten()
         anorm = a / a.sum()
         bnorm = b / b.sum()
-        X_pot, log = ot.sinkhorn(anorm.flatten(), bnorm.flatten(), C, reg=eta, log=True)
+        X_pot, log = ot.sinkhorn(anorm.flatten(), bnorm, C, reg=eta, log=True)
 
         beta_pot = eta * np.log(log['v'])
+        # beta_pot = np.log(log['v'])
         beta_pot = beta_pot / norm1(b) - np.sum(beta_pot*b) / norm1(b)**2
 
         beta_pot = beta_pot.reshape(-1, 1)
@@ -107,31 +113,44 @@ def linf(x):
 
 
 def update(b, beta, gamma=0.1):
-    res = b - gamma * beta
-    res[res < 0] = 0
-    return res 
+    return b - gamma * beta
 
-def update_n(b, med, n_iter=10, gamma=0.1):
+def update_n(b_init, med, n_iter=100, lr=0.1):
+    b = copy(b_init)
     for t in range(n_iter):
         beta = get_beta(b, med)
-        b = update(b, beta, gamma)
+        b = update(b, beta, lr)
 
+    b[b <= 0] = 1e-16
     return b
         
 
-b_l2 = update_n(b, med='l2')
-b_kl = update_n(b, med='kl')
-b_ot = update_n(b, med='ot')
-b_pot = update_n(b, med='pot')
-b_l1 = update_n(b, med='l1')
+b_l2 = update_n(b_init, med='l2')
+X = fw(C, a, b_l2, tau=tau)
+X[X < 1e-16] = 0
+print('X')
+print(coo_matrix(X))
+
+print('row sum (=a)')
+print(X.sum(1).reshape(5,5))
+
+print('column sum (=b)')
+print(X.sum(0).reshape(5,5))
+
+b_kl = update_n(b_init, med='kl')
+b_ot = update_n(b_init, med='ot')
+b_ot = b_ot / b_ot.sum()
+b_pot = update_n(b_init, med='pot')
+b_pot = b_pot / b_pot.sum()
+b_l1 = update_n(b_init, med='l1')
 
 fig, ax = plt.subplots(1, 7)
 fig.set_size_inches(9, 3)
-ax[0].imshow(a.reshape(5, 5))
+# ax[0].imshow(a.reshape(5, 5))
 ax[0].set_title('a')
 im_text(ax[0], a.reshape(5,5))
 
-ax[1].imshow(b.reshape(5, 5))
+# ax[1].imshow(b.reshape(5, 5).T)
 ax[1].set_title('b')
 im_text(ax[1], b.reshape(5,5))
 

lib/l2_semi_uot.py
@@ -7,6 +7,7 @@ import pdb
 from copy import copy
 import cvxpy as cp
 import time
+from scipy.sparse import coo_matrix
 
 
 def norm2sq(X):
@@ -15,7 +16,7 @@ def norm2sq(X):
 
 def fval(C, a, b, tau, X):
     r, c = C.shape
-    return np.sum(C * X) + tau / 2. * norm2sq(X - b.T)
+    return np.sum(C * X) + tau / 2. * norm2sq(X.T.sum(axis=-1, keepdims=True) - b)
 
 
 def fdual(C, a, b, tau, X):
@@ -35,7 +36,7 @@ def grad(C, b, X, tau):
     """
 
     r, c = C.shape
-    return C + tau * (X - b.T)
+    return C + tau * (X.sum(axis=0, keepdims=True) - b.T)
 
 
 def prox(X, G, L, a):
@@ -43,18 +44,25 @@ def prox(X, G, L, a):
     X: [n_a, n_b]
     G: [n_a, n_b]
     """
-    Z = X - (0.9 / L) * G
+    Z = X - (0.99 / L) * G
     P = projection_simplex(Z, a, axis=1)
     # print(f'X=X}, G={G}, Z={Z}, P={P}')
     return P
 
 
-def a_pgd(C, a, b, tau, n_iter=100, X=None, duals=False):
+def a_pgd(C, a, b, tau, n_iter=100, X=None, duals=False, debug=False):
+
+    Xs = []
+
     if X is None:
-        X = np.random.rand(*C.shape) + 1e-2
+        X = np.random.rand(*C.shape)
         X = projection_simplex(X, a, axis=1)
+
+        if debug:
+            Xs.append(X)
+
     t = 1.
-    Y = X
+    Y = copy(X)
     for i in range(n_iter):
         G = grad(C, b, Y, tau)
         XX = prox(Y, G, tau, a)
@@ -62,7 +70,13 @@ def a_pgd(C, a, b, tau, n_iter=100, X=None, duals=False):
         Y = XX + ((t - 1) / tt) * (XX - X)
         
         t = tt
-        X = XX
+        X = copy(XX)
+
+        if debug:
+            Xs.append(X)
+
+    if debug:
+        return Xs
 
     if duals:
         beta = tau * (b - X.T.sum(axis=-1, keepdims=True))
@@ -70,17 +84,29 @@ def a_pgd(C, a, b, tau, n_iter=100, X=None, duals=False):
     else:
         return X
 
-def pgd(C, a, b, tau, gamma=0.1, n_iter=100, X=None, duals=False):
+
+def pgd(C, a, b, tau, gamma=0.1, n_iter=100, X=None, duals=False, debug=False):
+
+    Xs = []
 
     if X is None:
         X = np.random.rand(*C.shape)
         X = projection_simplex(X, a, axis=1)
+
+        if debug:
+            Xs.append(X)
 
     for t in range(n_iter):
         G = grad(C, b, X, tau)
         X = X - gamma * G
         X = projection_simplex(X, a, axis=1)
 
+        if debug:
+            Xs.append(X)
+
+    if debug:
+        return Xs
+
     if duals:
         beta = tau * (b - X.T.sum(axis=-1, keepdims=True))
         return X, beta
@@ -88,13 +114,18 @@ def pgd(C, a, b, tau, gamma=0.1, n_iter=100, X=None, duals=False):
         return X
 
 
-def fw(C, a, b, tau, n_iter=100, X=None, duals=False):
+def fw(C, a, b, tau, n_iter=100, X=None, duals=False, debug=False):
     r, c = C.shape
+
+    Xs = []
 
     if X is None:
         X = np.random.rand(*C.shape)
         X = X / X.sum(axis=-1, keepdims=True)
         X = a * X # normalize
+
+        if debug:
+            Xs.append(X)
 
     for t in range(n_iter):
         gamma = 2 / (t + 2)
@@ -102,6 +133,12 @@ def fw(C, a, b, tau, n_iter=100, X=None, duals=False):
         idx = np.argmin(G, axis=-1)
         X = (1 - gamma) * X
         X[np.arange(r), idx] = X[np.arange(r), idx] + gamma * a.flatten()
+
+        if debug:
+            Xs.append(X)
+
+    if debug:
+        return Xs
 
     if duals:
         beta = tau * (b - X.T.sum(axis=-1, keepdims=True))
@@ -115,7 +152,7 @@ def exact(C, a, b, tau):
     X = cp.Variable(C.shape, nonneg=True)
     constraints = [cp.sum(X, axis=1, keepdims=True) == a]
     obj = cp.sum(cp.multiply(X, C))
-    obj += tau / 2 * cp.sum_squares(X - np.ones((r, 1)) @ b.T)
+    obj += tau / 2 * cp.sum_squares(cp.sum(X, axis=0, keepdims=True) - b.T)
     prob = cp.Problem(cp.Minimize(obj), constraints)
 
     result = prob.solve(solver='SCS')
@@ -124,31 +161,49 @@ def exact(C, a, b, tau):
 if __name__ == '__main__':
     np.random.seed(0)
     a = np.random.rand(200,1)
-    b = np.random.rand(4000,1)
-    C = np.random.rand(200, 4000)
-    # C = (C + C.T) / 2
-    tau = 1.
-    n_iter = 5
+    b = np.random.rand(400,1)
+    C = np.random.rand(200, 400)
+    # a = np.array([0.25, 0.75]).reshape(-1, 1)
+    # b = np.array([0.75, 0.25]).reshape(-1, 1)
+    # C = np.array([[0,1],[1,0]])
+    # # C = (C + C.T) / 2
+    tau = 10.
+    n_iter = 100
+
+    # t_s = time.time()
+    # Xs = a_pgd(C, a, b, tau, n_iter=n_iter, debug=True)
+    # print(time.time() - t_s)
+    # fs = [fval(C, a, b, tau, X) for X in Xs]
+    # fds = [fdual(C, a, b, tau, X) for X in Xs]
+    # plt.plot(np.arange(n_iter+1), fs)
+    # plt.plot(np.arange(n_iter+1), fds)
+    # plt.show()
+    # print(Xs[-1])
 
     t_s = time.time()
-    X = a_pgd(C, a, b, tau, n_iter=n_iter)
+    Xs = pgd(C, a, b, tau, gamma=0.99/tau, n_iter=n_iter, debug=True)
     print(time.time() - t_s)
-    fs = fval(C, a, b, tau, X)
+    fs = [fval(C, a, b, tau, X) for X in Xs]
+    fds = [fdual(C, a, b, tau, X) for X in Xs]
+    plt.plot(np.arange(n_iter+1), fs)
+    plt.plot(np.arange(n_iter+1), fds)
     print(fs)
+    print(Xs[-1])
 
     t_s = time.time()
-    X = pgd(C, a, b, tau, gamma=0.9/tau, n_iter=n_iter)
+    Xs = fw(C, a, b, tau, n_iter=n_iter, debug=True)
     print(time.time() - t_s)
-    fs = fval(C, a, b, tau, X)
-    print(fs)
+    fs = [fval(C, a, b, tau, X) for X in Xs]
+    fds = [fdual(C, a, b, tau, X) for X in Xs]
+    plt.plot(np.arange(n_iter+1), fs)
+    plt.plot(np.arange(n_iter+1), fds)
+    print(fs[-1])
+    print(np.count_nonzero(Xs[-1]))
 
-    t_s = time.time()
-    X = fw(C, a, b, tau, n_iter=100)
-    print(time.time() - t_s)
-    fs = fval(C, a, b, tau, X)
-    print(fs)
+    ex_res, ex_X = exact(C, a, b, tau)
 
-    # ex_res, ex_X = exact(C, a, b, tau)
+    print(f'exact: f={ex_res:.3f}, X={ex_X}')
+    print(ex_res)
+    print(np.count_nonzero(ex_X))
 
-    # print(f'exact: f={ex_res:.3f}, X={ex_X}')
-    # print(ex_res)
+    plt.show()

lib/test_gradient.py
@@ -44,9 +44,9 @@ a = a.reshape(25, 1)
 # supp_a = np.array([[0,2], [4,4]])
 # a = np.array([1., 1.]).reshape(-1, 1)
 b = np.zeros((5, 5))
-b[0, 1] = 1.
-b[4, 3] = 1.
-b[4, 0] = 1.
+b[0, 1] = 2.
+b[4, 3] = 3.
+b[4, 0] = 4.
 b = b.reshape(25, 1)
 a = a + 1e-16
 b = b + 1e-16
@@ -59,9 +59,9 @@ supp = np.concatenate((XX.reshape(-1,1), YY.reshape(-1,1)), axis=1)
 C = cdist(supp, supp, 'euclidean')**2
 
 eta = 0.001
-tau = 100.
+tau = 0.1
 
-X, beta_l2 = pgd(C, a, b, tau, duals=True)
+X, beta_l2 = fw(C, a, b, tau, duals=True)
 beta_l2 = beta_l2 / tau
 X, _, _ = kl_sinkhorn(C, a, b, eta, tau, duals=True)
 beta_kl = - X.T.sum(axis=-1, keepdims=True) / b + 1
@@ -74,8 +74,8 @@ beta_ot = beta_ot / norm1(b) - np.sum(beta_ot*b) / norm1(b)**2
 X_pot, log = ot.sinkhorn(anorm.flatten(), bnorm.flatten(), C, reg=eta, log=True)
 
 beta_pot = eta * np.log(log['v'])
-beta_pot = beta_pot / norm1(b) - np.sum(beta_pot*b) / norm1(b)**2
 beta_pot = beta_pot.reshape(-1, 1)
+beta_pot = beta_pot / norm1(b) - np.sum(beta_pot*b) / norm1(b)**2
 
 
 beta_l1 = np.zeros(b.shape)
@@ -109,23 +109,23 @@ ax[1].set_title('b')
 im_text(ax[1], b.reshape(5,5))
 
 # ax[2].imshow(beta_l2.reshape(5, 5), cmap='plasma')
-im_text(ax[2], update(b, beta_l2).reshape(5, 5))
+im_text(ax[2], beta_l2.reshape(5, 5))
 ax[2].set_title(f'l2_pgd_{linf(beta_l2):.3f}')
 
 # ax[3].imshow(beta_kl.reshape(5, 5), cmap='plasma')
-im_text(ax[3], update(b, beta_kl).reshape(5, 5))
+im_text(ax[3], beta_kl.reshape(5, 5))
 ax[3].set_title(f'kl_uot_{linf(beta_kl):.3f}')
 
 # ax[4].imshow(beta_ot.reshape(5, 5), cmap='plasma')
-im_text(ax[4], update(b, beta_ot).reshape(5, 5))
+im_text(ax[4], beta_ot.reshape(5, 5))
 ax[4].set_title(f'ot_(mine)_{linf(beta_ot):.3f}')
 
 # ax[5].imshow(beta_pot.reshape(5, 5), cmap='plasma')
-im_text(ax[5], update(b, beta_pot).reshape(5, 5))
+im_text(ax[5], beta_pot.reshape(5, 5))
 ax[5].set_title(f'pot_{linf(beta_pot):.3f}')
 
 # ax[6].imshow(beta_l1.reshape(5, 5), cmap='plasma')
-im_text(ax[6], update(b, beta_l1).reshape(5, 5))
+im_text(ax[6], beta_l1.reshape(5, 5))
 ax[6].set_title(f'l1_{linf(beta_l1):.3f}')
 
 # fig.colorbar(im, ax=ax[5])